b4a37ccf97a5b60ce6a6593deac2e390c4896c3f
Andreas <andreasb6d@gmx.de>
2020-12-15 08:46:28 +0100

    commit maineinzel.py

--- maineinzel.py
+++ maineinzel.py
@@ -5,16 +5,19 @@ import damageDetection as dd
 import hilfsfunktionen as hf
 import numpy as np
 
-substrate = "glas"
+substrate = "heute"
 picture = "1"
 
 path = ''.join([substrate, "/", picture, ".png"])
 img = cv2.imread(path)
 
+x = int(img.shape[1] / 2)  # Breite
+y = int(img.shape[0] / 2)  # Höhe
+#img = cv2.resize(img,(x,y))
 #img = hf.quarter(img,1)
 #img = img[50:img.shape[0]-50,50:img.shape[1]-50]
 
-ergebnis,img,coordinates = dd.damageDetection(img,substrate)
+ergebnis,img,coordinates = dd.damageDetection(img,"sandwich2")
 
 hf.klein("img",img)
 
